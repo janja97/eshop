8ea8968e75f619d495e61e0ab85fa3b6c5a6e592
janja97 <97881824+janja97@users.noreply.github.com>
2022-02-04 20:06:38 +0100

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -16,13 +16,13 @@ if(Session::has('user')){
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="/">tehnoshop</a>
+        <a class="navbar-brand" href="{{url('/index')}}">tehnoshop</a>
       </div>
   
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="/" >Home</a></li>
+          <li class="active"><a href="{{url('/index')}}" >Home</a></li>
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">IT OPREMA <span class="caret"></span></a>
             <ul class="dropdown-menu">
