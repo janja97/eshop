d1aead3bbecca953b453b8c99556d7adcf25d2c6
janja97 <97881824+janja97@users.noreply.github.com>
2022-02-05 20:48:24 +0100

    Add files via upload

---
+++ resources/views/laptop.blade.php
@@ -0,0 +1,25 @@
+@extends('master')
+@section('content')
+<div class="custom-product">
+  
+    
+    {{-- popularni proizvodi --}}
+    <div class="treding-wrapper container">
+        <h3>Laptopi:</h3>
+        @foreach ($product as $item)
+            
+              <div class="treding-item">
+                <a href="detail/{{$item['id']}}">
+                <img class="treding-img" src="{{$item['galery']}}">
+                <div class="">
+                    <h4>{{$item['name']}}</h4>
+                </div>
+                </a>
+              </div>
+             
+        @endforeach
+    </div>
+    </div>
+    
+</div>
+@endsection
